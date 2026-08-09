0889fc62d077e861ee7263d4583446f3e8a50750
Fatemeh (Saba) Ganji <109040494+WPISabaGanji@users.noreply.github.com>
2026-08-09 00:43:07 -0400

    Remove people photos from homepage carousel

--- src/components/SimpleSlider.js
+++ src/components/SimpleSlider.js
@@ -23,15 +23,6 @@ function SimpleSlider(props) {
             >
                 <div className="carousel-item-padding-40-px"><img src="/img/slide/alp1.JPG" alt="Slide 1" className="standard-image-in-carousel"/></div>
                 <div className="carousel-item-padding-40-px"><img src="/img/slide/alp2.JPG" alt="Slide 2" className="standard-image-in-carousel"/></div>
-                <div className="carousel-item-padding-40-px"><img src="/img/slide/alp3.JPG" alt="Slide 3" className="standard-image-in-carousel"/></div>
-                <div className="carousel-item-padding-40-px"><img src="/img/slide/conf.JPG" alt="Slide 4" className="standard-image-in-carousel"/></div>
-                <div className="carousel-item-padding-40-px"><img src="/img/slide/group.jpg" alt="Slide 5" className="standard-image-in-carousel"/></div>
-                <div className="carousel-item-padding-40-px"><img src="/img/slide/hh.jpg" alt="Slide 6" className="standard-image-in-carousel"/></div>
-                <div className="carousel-item-padding-40-px"><img src="/img/slide/MJB_8714.jpg" alt="Slide 7" className="standard-image-in-carousel"/></div>
-                <div className="carousel-item-padding-40-px"><img src="/img/slide/phem1.jpg" alt="Slide 8" className="standard-image-in-carousel"/></div>
-                <div className="carousel-item-padding-40-px"><img src="/img/slide/pres.JPG" alt="Slide 9" className="standard-image-in-carousel"/></div>
-                <div className="carousel-item-padding-40-px"><img src="/img/slide/sca.png" alt="Slide 10" className="standard-image-in-carousel"/></div>
-                <div className="carousel-item-padding-40-px"><img src="/img/slide/debug.png" alt="Slide 11" className="standard-image-in-carousel"/></div>
             </Carousel>
 
             <div className="hero-text-banner">
